fc2b209fbce4bd49edafdde5ea5361b9d22f181c
tijothomas <tijothomas366@gmail.com>
2025-02-19 19:34:52 +0530

    04-create-home-page-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,5 +33,40 @@ const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'Farm-Fresh Goodness Delivered to Your Doorstep!',
+
+      buttonCaption: 'Shop Now',
+
+      image: '/images/banner1.jpg',
+
+      url: '/search?category=Shoes',
+
+      isPublished: true,
+    },
+    {
+      title: 'Tasty Treats, Anytime! ',
+
+      buttonCaption: 'Shop Now',
+
+      image: '/images/banner2.jpg',
+
+      url: '/search?category=T-Shirts',
+
+      isPublished: true,
+    },
+    {
+      title: 'From Bold to Natural – We’ve Got Your Perfect Shade!',
+
+      buttonCaption: 'See More',
+
+      image: '/images/banner3.jpg',
+
+      url: '/search?category=Wrist Watches',
+
+      isPublished: true,
+    },
+  ],
 }
 export default data
