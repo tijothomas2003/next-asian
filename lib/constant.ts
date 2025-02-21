33aea3a9089d40e386c64a00f328580818e07e63
tijothomas <tijothomas366@gmail.com>
2025-02-21 19:54:08 +0530

    09-create-product-details-page

--- lib/constant.ts
+++ lib/constant.ts
@@ -4,3 +4,4 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An Online Store for all asian groceries'
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
